8ae898c3a53ef2bfc91345ef19d36874d61e76c8
david <dmw2442@outlook.com>
2021-05-11 17:43:29 -0400

    initial dev commit

---
+++ days_on_market.py
@@ -0,0 +1,9 @@
+#finds the average days on market for a dataframe 
+
+def mean_dom(df):
+    #finds the days on market 
+    pass 
+
+def median_dom(df):
+    #finds the median days on market
+    pass 
